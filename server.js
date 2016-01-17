979ea911ba430f0fe5be252f54361f809e947d14
Gabriel <gabriel.bira95@gmail.com>
2016-01-17 20:53:55 +0000

    Implemented the link saving part.

--- server.js
+++ server.js
@@ -7,23 +7,49 @@ var bodyParser=require('body-parser');
 var Router=require('router');
 var router=Router();
 var app = express();
+var validate = require('url-validator');
 
 app.use(router);
 mongoose.connect('mongodb://localhost:27017/clementinejs');
+
 var urlSchema = mongoose.Schema({
-    id : Number,
     url: String
 });
 
 var url = mongoose.model('url', urlSchema);
-var validate = require('url-validator');
 
 router.get('/',function(req,res){
-    res.send('Chello!');
+    res.end('Chello!');
 });
 
+var error={};
+
 router.get('/new/:url(*)', function(req,res){
-    res.send(req.params.url);
+    var inputUrl=req.params.url;
+    if(!validate(inputUrl)){      //if invalid
+        res.json(error);                    //return error
+        return;
+    }
+    url.find({url : inputUrl}, function(err,data){
+        if(err)
+            throw err;
+        if(data==null){
+            var saveUrl = new url( { url:inputUrl } );
+            saveUrl.save(function(err, saveUrl){
+                if(err)
+                    console.log(err);
+                res.json(saveUrl);
+                console.log(inputUrl);
+                return;
+            });
+        }
+        else{
+            res.json(data);
+        }
+    });
+    //else
+        //create item in db
+        //return item in db
 });
 
 var port = process.env.PORT || 8080;
